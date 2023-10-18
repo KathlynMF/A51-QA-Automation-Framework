3affc5d28d77a97706d056e5987f71f00d0df3ef
KathlynMF <147891760+KathlynMF@users.noreply.github.com>
2023-10-17 18:02:47 -0700

    Add files via upload

---
+++ Homework01Primitives.java
@@ -0,0 +1,55 @@
+class Main {
+  public static void main(String[] args) {
+
+    /*
+     * Homework #1 - Primitives
+     * Write a simple Java application to finish the assignments below
+     * 
+     * Assignment #1
+     * 1.) Print your name
+     */
+   
+    System.out.println("Assignment #1");
+    // Write your answer below this line
+     System.out.println("Kathlyn Miller-Francis");
+
+    /*
+     * Assignment #2
+     * 1.) Declare an int variable named 'bookPrice' equal to $45
+     * Example: int bookPrice = 45;
+     * 2.) Declare a double variable named 'discount' equal to 20% in decimal format
+     * 3.) Declare a double variable named 'total' that will calculate the
+     * discounted price of the book
+     * 4.) Print out the result of total due by a customer when purchasing a book
+     * that costs $45 with a 20% discount
+     */
+    System.out.println("Assignment #2");
+    // Write your answer below this line
+       int bookPrice = 45;
+       double discount = 0.2;
+       double total = bookPrice - bookPrice * discount;
+       System.out.println(total);
+    /*
+     * Assignment #3
+     * 1.) Convert 90 degrees Fahrenheit to Celsius
+     * 2.) Use the following formula: C = (F − 32) × 5/9) to calculate it
+     * 3.) Print out the Celsius convertion without declaring any variables
+     */
+    System.out.println("Assignment #3");
+    // Write your answer below this line
+    System.out.println("Convert 90 degrees Fahrenheit to Celsius:");
+
+   System.out.println((90 - 32) * 5 / 9);
+    // Check the answers and submit your homework by clicking 'Submit' button at the top right
+  }
+}
+//messsage from home work team
+/*I have reviewed your Java code for the assignments, and I'm pleased to inform you that you have followed the instructions correctly. Your code successfully completes all three assignments:
+
+Assignment #1: You printed your name as instructed.
+
+Assignment #2: You declared variables for book price and discount, calculated the discounted price accurately, and printed the result.
+
+Assignment #3: You correctly converted 90 degrees Fahrenheit to Celsius using the provided formula and displayed the result.
+
+Your code demonstrates a good understanding of the tasks and Java syntax. Keep up the good work! If you have any more assignments or questions in the future, don't hesitate to ask for assistance.*/
