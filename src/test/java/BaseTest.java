589620a386ac5b4dfae84dfa7a5eb6a6b1ba1613
KathlynMF <147891760+KathlynMF@users.noreply.github.com>
2023-10-30 20:00:28 -0700

    Homework 18 final commit
    commit new branch code

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,30 @@
+<<<<<<< Updated upstream
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
+=======
+
+import org.openqa.selenium.WebDriver;
+>>>>>>> Stashed changes
 import org.testng.annotations.BeforeSuite;
+
 
 public class BaseTest {
 
+    public WebDriver driver = null;
+
+    //@Test(dataProvider = "LoginData")
+    public void navigateToPage(String url){
+        driver.get(url);
+    }
+
     @BeforeSuite
     static void setupClass() {
+<<<<<<< Updated upstream
         WebDriverManager.chromedriver().setup();
+=======
+        Object WebDriverManager = new Object();
+        WebDriverManager.toString()
+
+>>>>>>> Stashed changes
     }
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
+=======
+
+import org.openqa.selenium.By;
+>>>>>>> Stashed changes
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -7,6 +13,8 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
+
+
     @Test
     public void loginEmptyEmailPassword() {
 
@@ -21,5 +29,34 @@ public class LoginTests extends BaseTest {
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
+    }
+
+    @Test
+    public void loginValidEmailPassword(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    @Test(enabled = false, description = "Test has been marked as skipped due to an ongoing issue, JIRA-757.")
+    public void loginInvalidEmailValidPassword(){
+
+        ChromeOptions options = new ChromeOptions();
+        var chromeOptions = options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        WebDriver.Timeouts timeouts = null;
+        timeouts.implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement emailField = driver.findElement(new By.ByCssSelector("input[type ='email']"));
+        emailField.clear();
+        emailField.sendKeys("invalid@class.com");
+
     }
 }
