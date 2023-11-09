e2a17d536e7b97c1cd154c4a5349024df5582b57
KathlynMF <147891760+KathlynMF@users.noreply.github.com>
2023-11-08 19:17:01 -0800

    push java and gradle
    Homework 23

--- src/test/java/Homework20.java
+++ src/test/java/Homework20.java
@@ -9,19 +9,50 @@ import org.testng.annotations.Test;
 
 public class Homework20 extends BaseTest {
 
-    String new PlaylistName = "Sample Edited Playlist";
+    @Test
+    public void hoverOverPlayButton(){
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clicksubmit();
+        Assert.assertTrue(hoverPlay().isDisplayed());
+    }
+
+    public WebElement hoverPlay(){
+        WebElement play = driver.findElement(By.cssSelector("[data-testid='play-btn]"));
+        actions.moveToElement(play).perform();
+        return wait.until(ExpectedConditions.visibilityOf(play));
+    }
 
     @Test
-    public void renamePlaylist(){
 
-        String updatedPlaylistMsg = "Updated playlist \"Sample Edited Playlist.\"";
+    public void countSongsInPlaylist(){
 
         provideEmail("demo@class.com");
-        providePassword("te$t$udent");
+        providePassword("te$t$tudent");
         clickSubmit();
-        doubleClickPlaylist();
-        enterNewPlaylistName();
-        Assert.assertEquals(getRenamePlaylistSuccessMsg(), updatedPlaylistMsg);
+        choosePlaylistByName("Playlist Demo");
+        displayALlSongs();
+        Assert.assertTrue(getPlaylistDetails().contains(Stromg.value0f(countSongs())));
+    }
+
+    public void choosePlaylistByName(String playlistName){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),']" + playlistName + ")]"))).click();
+    }
+
+    public int countSongs() {
+        return driver.findELement(By.cssSelector("section#playlistWrapper td.title")).size();
+    }
+
+    public String getPlaylistDetails(){
+        return driver.findElement(By.cssSelector("span.meta.text-secondary span.meta")).getText();
+    }
+
+    public void displayAllSongs(){
+        List<WebElement> songList = driver.findElement(By.cssSelector("section#playlistWrapper td.title"));
+        System.out.println("Number of songs found: " + countSongs());
+        for (WebElement e: songList){'
+        System.out.println(e.getTest());
+        }
     }
 
 }
